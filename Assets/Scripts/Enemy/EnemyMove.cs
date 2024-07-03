0a724c7999d1bf131c2d97ea4c958c6d25f9c3c2
ZZalbs <57441250+ZZalbs@users.noreply.github.com>
2024-07-03 13:45:45 +0900

    enemymove 이동 코드 작성 완료
    테스트 추가하자

--- Assets/Scripts/Enemy/EnemyMove.cs
+++ Assets/Scripts/Enemy/EnemyMove.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
+    private Rigidbody2D enemyRb;
 
+    private float moveSpeed = 2f;
+
+    private Transform targetMovePos;
+
+    private int wayPointNum;
 
     void Start()
     {
-        
+        targetMovePos = EnemyWaypoints.pub_Instance().points[wayPointNum];
     }
     void Update()
     {
-        
+        if(Vector2.Distance(targetMovePos.position,transform.position)< 0.05f)
+        {
+            wayPointNum++;
+        }
+        Vector2 dir = (targetMovePos.position - transform.position).normalized;
+        transform.Translate(dir * moveSpeed * Time.deltaTime);
     }
 }
--- Assets/Scripts/Enemy/EnemyWaypoints.cs
+++ Assets/Scripts/Enemy/EnemyWaypoints.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 
 public class EnemyWaypoints : MonoBehaviour
 {
-    //싱글톤
+    //싱글톤. 이거 나중에 게임매니저에 합치기
+    //https://velog.io/@xoaud321/Unity-%EC%9C%A0%EB%8B%88%ED%8B%B0-%EC%8B%B1%EA%B8%80%ED%84%B4-%ED%8C%A8%ED%84%B4-%EC%82%AC%EC%9A%A9%ED%95%B4-%EB%A7%90%EC%95%84-Singleton-Pattern
+    //참고
     private static EnemyWaypoints priv_instance;
-    public EnemyWaypoints pub_Instance()
+    public static EnemyWaypoints pub_Instance()
     {
         if(priv_instance == null)
         {
