ea08e3832bbab6eba24082b391e48182bd65c6ac
Zzalbs <57441250+ZZalbs@users.noreply.github.com>
2024-12-19 00:19:26 +0900

    인스펙터 안뜨는 오류 해결

--- Assets/Scripts/Shop/TowerInfo.cs
+++ Assets/Scripts/Shop/TowerInfo.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TowerInfo
+public class TowerInfo:MonoBehaviour
 {
     [SerializeField]
     private string towerName;
