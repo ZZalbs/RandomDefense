72e45ab7e15e3d68a89d87787103cf21e29d6f17
Zzalbs <57441250+ZZalbs@users.noreply.github.com>
2024-12-22 00:19:10 +0900

    상점 3개뽑기 함수 추가

--- Assets/Scripts/Shop/ShopManager.cs
+++ Assets/Scripts/Shop/ShopManager.cs
@@ -1,19 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopManager : MonoBehaviour
 {
     [SerializeField]
     public List<TowerInfo> towerList = new List<TowerInfo>();
     // 현재는 public 에서 입력하는 방식. 나중에 scriptableObject에서 데이터 받는걸로 바꾸기
+    public Canvas shopUI;
+    List<TowerInfo> pickedTower = new List<TowerInfo>();
+
+    public void ShowPickedTowerUI()
+    {
+
+    }
+
+    public List<TowerInfo> PickThreeTower(int curLevel)
+    {
+        pickedTower.Clear();
+        for(int i=0;i<3;i++)
+            pickedTower.Add(PickRandomTower(curLevel));
+        return pickedTower;
+    }
 
     TowerInfo PickRandomTower(int curLevel)
     {
         return towerList[getRandomNaturalNum(curLevel)];
-        
     }
-
+    
     int getRandomNaturalNum(int max)
     {
         return Random.Range(0, max)+1;
